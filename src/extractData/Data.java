5df2208628bf328e8f0b4317585bb34c8f500b92
el-tonino <92928031+el-tonino@users.noreply.github.com>
2022-10-06 16:22:55 +0200

    création d'une fonction de génération qui génère des phrases selon la chaine de markov

---
+++ src/extractData/Data.java
@@ -0,0 +1,71 @@
+package extractData;
+
+import java.io.*;
+import java.util.*;
+
+public class Data {
+
+    public static void init () {
+        init_used(false);
+    }
+
+    public static final HashMap<String, HashMap<String, Integer>> relations = new HashMap<>();
+
+    private static void init_used(boolean json) {
+        List<String> mots = mots();
+
+        for (int i = 0; i < mots.size() - 1; i++) {
+
+            if (relations.containsKey(mots.get(i))) {
+                HashMap<String, Integer> current = relations.get(mots.get(i));
+
+                if (!current.containsKey(mots.get(i + 1))) {
+                    current.put(mots.get(i + 1), 1);
+                } else {
+                    Integer value = current.get(mots.get(i + 1)) + 1;
+                    current.put(mots.get(i + 1), value);
+                }
+            } else {
+                HashMap<String, Integer> a_ajouter = new HashMap<>();
+                a_ajouter.put(mots.get(i + 1), 1);
+                relations.put(mots.get(i), a_ajouter);
+            }
+        }
+    }
+
+    private static List<String> mots() {
+        // Le fichier d'entrée
+        File file = new File("src/data/data.txt");
+        // Créer l'objet File Reader
+        FileReader fr = null;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        // Créer l'objet BufferedReader
+        BufferedReader br = new BufferedReader(fr);
+        StringBuffer sb = new StringBuffer();
+        String line;
+        while (true) {
+            try {
+                if ((line = br.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // ajoute la ligne au buffer
+            sb.append(line);
+            sb.append("\n");
+        }
+        try {
+            fr.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        String s = sb.toString();
+        // TODO changer le regex pour que le split se fasse sur les epscaes et les virgules
+        return List.of(s.split("[ ;]"));
+    }
+}
--- src/main/MarkovChain.java
+++ src/main/MarkovChain.java
@@ -1,5 +1,71 @@
 package main;
 
+import extractData.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 public class MarkovChain {
 
+    // private HashMap<String, Integer> indices_probas = new HashMap<>();
+
+
+    private static String generate_recursive(String firstWord, int numberOfWords){
+
+        if (numberOfWords <= 0)
+            return "";
+
+        HashMap<ArrayList<Integer>, String> indices_probas = new HashMap<>();
+
+        if(!Data.relations.containsKey(firstWord))
+            throw new Error("le mot " + firstWord + " n'existe pas dans les données");
+
+        HashMap<String, Integer> start = Data.relations.get(firstWord);
+
+        Set<String> s = Data.relations.get(firstWord).keySet();
+
+        int somme = 0;
+        for (String i : s){
+            int old_somme = somme+1;
+            somme += Data.relations.get(firstWord).get(i)+1;
+            ArrayList<Integer> intervalle = new ArrayList<>();
+            intervalle.add(old_somme);
+            intervalle.add(somme);
+            indices_probas.put(intervalle, i);
+        }
+        int min = 1;
+        int max = somme;
+
+        int rand = (int) (min + (Math.random() * (max+1 - min)));
+
+        String nextWord = "";
+
+        for (ArrayList<Integer> i : indices_probas.keySet()){
+            if(rand >= i.get(0) && rand <= i.get(1)){
+                nextWord = indices_probas.get(i);
+            }
+        }
+
+        return nextWord + " " + generate_recursive(nextWord, numberOfWords-1);
+    }
+
+    private static String generate(String firstWord, int numberOfWords){
+        return firstWord + generate_recursive(firstWord, numberOfWords-1);
+    }
+    public static void main(String[] args) {
+        Data.init();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("premier mot : ");
+            String response_1 = sc.next();
+            System.out.println("nombre de mots : ");
+            String response_2 = sc.next();
+
+            System.out.println("\n" + generate(response_1, Integer.parseInt(response_2)));
+            System.out.println("\n\n");
+        }
+    }
 }
