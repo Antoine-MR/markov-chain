972224effa63106de29fe99756f642069ac0058e
el-tonino <92928031+el-tonino@users.noreply.github.com>
2022-10-07 19:33:29 +0200

    json créé

--- src/main/java/extractData/Data.java
+++ src/main/java/extractData/Data.java
@@ -1,22 +1,64 @@
 package extractData;
 
+import com.google.gson.*;
+
 import java.io.*;
 import java.util.*;
 
 public class Data {
 
-    public static void init () {
-        init_used(false);
-    }
+    public static String txtName = "demo.txt";
+    public static String jsonName = "probabilities.json";
+    public static String txtPath = "src/main/java/data/txt/english/"+ txtName;
+    public static String jsonPath = "src/main/java/data/" + jsonName;
+
+    public static final JsonObject json_relations = new JsonObject();
 
     public static final HashMap<String, HashMap<String, Integer>> relations = new HashMap<>();
 
-    private static void init_used(boolean json) {
+    public static JsonObject getJson(){
+        File file = new File(jsonPath);
+        FileReader reader;
+        try {
+            reader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Gson gson = new Gson();
+        JsonObject obj = gson.fromJson(new BufferedReader(reader), JsonObject.class);
+        return obj;
+    }
+
+    public static void init () {
+        File file = new File(jsonPath);
+        FileReader reader;
+        try {
+            reader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            if (new BufferedReader(reader).readLine() == null){
+                init_with_txt();
+            }
+            else {
+                init_with_json();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void init_with_txt() {
         List<String> mots = mots();
+        // JsonArray json_mots =
 
         for (int i = 0; i < mots.size() - 1; i++) {
 
             if (relations.containsKey(mots.get(i))) {
+
                 HashMap<String, Integer> current = relations.get(mots.get(i));
 
                 if (!current.containsKey(mots.get(i + 1))) {
@@ -31,11 +73,19 @@ public class Data {
                 relations.put(mots.get(i), a_ajouter);
             }
         }
+        writeJSON();
+    }
+
+    private static void init_with_json(){
+        Gson gson = new Gson();
+        JsonObject obj = getJson();
+        Set<Map.Entry<String, JsonElement>> s = obj.entrySet();
+        //System.out.println(s);
     }
 
     private static List<String> mots() {
         // Le fichier d'entrée
-        File file = new File("src/main/java/data/data.txt");
+        File file = new File(txtPath);
         // Créer l'objet File Reader
         FileReader fr;
         try {
@@ -67,5 +117,26 @@ public class Data {
         String s = sb.toString();
         // TODO changer le regex pour que le split se fasse sur les epscaes et les virgules
         return List.of(s.split("[ ;]"));
+    }
+
+    private static void writeJSON(){
+
+        Gson gson = new Gson();
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(jsonPath, "UTF-8");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        String jo = gson.toJson(relations);
+        writer.println(jo);
+        writer.close();
+    }
+
+    public static void main(String[] args) {
+        init();
+        //writeJSON();
     }
 }
